13360bc5050176e94cab77910a710796627575e9
Erispoe <favrebulle@gmail.com>
2014-05-22 16:05:06 +0200

    Export JSON

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
 	// Check connection
 	if (mysqli_connect_errno()) {
   		echo "Failed to connect to MySQL: " . mysqli_connect_error() . "</br>";
-	} else {
-		echo "Connected to database" . "</br>";
-	}
+	};
 
 	$zip = $_GET['zip'];
-	echo $zip . "</br>";
 
 	$result = mysqli_query($con,"SELECT * FROM A_ZipCBSA_2010 WHERE zip = " . $zip);
 
 	while($row = mysqli_fetch_array($result)) {
-  		print_r($row);
-  		echo $row['countyname'] . " - " . $row['CBSATitle'];
-  		echo "</br>";
+  		//print_r($row);
+  		$r = array( 'zip' => $row['zip'],
+  					'countyname' => $row['countyname'],
+  					'CBSATitle' => $row['CBSATitle'],
+  					'CBSACentralCity' => $row['CBSACentralCity']);
+  		echo json_encode($r);
 	}
 
 	mysqli_close($con);
