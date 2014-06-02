34f2320917b9c8d9cf55efffadb9fded183fb8fd
Erispoe <favrebulle@gmail.com>
2014-06-02 14:45:36 +0200

    Add new fields.

--- index.php
+++ index.php
@@ -6,33 +6,36 @@
 	// Create connection
 	$con=mysqli_connect("localhost","root","root","metroatlas");
 
-	// Check connection
-	if (mysqli_connect_errno()) {
-  		echo "Failed to connect to MySQL: " . mysqli_connect_error() . "</br>";
-	};
+  // Check connection
+  if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error() . "</br>";
+  };
 
-	$zip = $_GET['zip'];
-	$field = $_GET['field'];
+  $zip = $_GET['zip'];
+  $field = $_GET['field'];
 
-	$result = mysqli_query($con,"SELECT * FROM A_ZipCBSA_2010 WHERE zip = " . $zip);
+  $result = mysqli_query($con,"SELECT * FROM A_ZipCBSA_2010 WHERE zip = " . $zip);
 
-	while($row = mysqli_fetch_array($result)) {
-  		//print_r($row);
-  		$r = array( 'zip' => $row['zip'],
-  					'countyname' => $row['countyname'],
-  					'CBSATitle' => $row['CBSATitle'],
-  					'CBSACentralCity' => $row['CBSACentralCity']
-            'StateName' => $row['StateName']
+  while($row = mysqli_fetch_array($result)) {
+      //print_r($row);
+      $r = array( 'zip' => $row['zip'],
+            'countyname' => $row['countyname'],
+            'countynamefull' => $row['countynamefull'],
+            'CBSATitle' => $row['CBSATitle'],
+            'CBSACentralCity' => $row['CBSACentralCity'],
+            'CBSACode' => $row['CBSACode'],
+            'StateName' => $row['StateName'],
             'CSATitle' => $row['CSATitle'],
+            'CSACode' => $row['CSACode'],
             'CSACentralCity' => $row['CSACentralCity']);
-  		if($field) {
-  			echo $r[$field];
-  		} else {
-  			echo json_encode($r);
-  		}
-  		
-	}
+      if($field) {
+        echo $r[$field];
+      } else {
+        echo json_encode($r);
+      }
+      
+  }
 
-	mysqli_close($con);
+  mysqli_close($con);
 
 ?>
