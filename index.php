7af074b2a89ca18a27de0a375c2cb1b984bd53d8
Erispoe <favrebulle@gmail.com>
2014-07-08 17:24:23 +0200

    Add list of cities to the general API

--- index.php
+++ index.php
@@ -18,38 +18,39 @@
   $userkey = $_GET['key'];
 
   if($userkey == $key){
-  	$result = mysqli_query($con,"SELECT * FROM A_ZipCBSA_2010 WHERE zip = " . $zip);
-	  while($row = mysqli_fetch_array($result)) {
-	      //print_r($row);
-	      $r = array( 'zip' => $row['zip'],
-	            'countyname' => $row['countyname'],
-	            'countynamefull' => $row['countynamefull'],
-	            'CBSATitle' => $row['CBSATitle'],
-	            'CBSACentralCity' => $row['CBSACentralCity'],
-	            'CBSACode' => $row['CBSACode'],
-	            'StateName' => $row['StateName'],
-	            'CSATitle' => $row['CSATitle'],
-	            'CSACode' => $row['CSACode'],
-	            'CSACentralCity' => $row['CSACentralCity'],
-	            'PSATitle' => $row['PSATitle'],
-	            'PSACode' => $row['PSACode'],
-	            'PSACentralCity' => $row['PSACentralCity']);
-	      if($field) {
-	        if($field == 'cities') {
-	          // Transform the allCities field in a json object
-	          $cities = explode(", ", $row['allCities']);
-	          echo json_encode($cities);
-	        } else {
-	          echo $r[$field];
-	        }
-	      } else {
-	        echo json_encode($r);
-	      }
-	      
-	  }
+    $result = mysqli_query($con,"SELECT * FROM A_ZipCBSA_2010 WHERE zip = " . $zip);
+    while($row = mysqli_fetch_array($result)) {
+        //print_r($row);
+        $r = array( 'zip' => $row['zip'],
+              'countyname' => $row['countyname'],
+              'countynamefull' => $row['countynamefull'],
+              'CBSATitle' => $row['CBSATitle'],
+              'CBSACentralCity' => $row['CBSACentralCity'],
+              'CBSACode' => $row['CBSACode'],
+              'StateName' => $row['StateName'],
+              'CSATitle' => $row['CSATitle'],
+              'CSACode' => $row['CSACode'],
+              'CSACentralCity' => $row['CSACentralCity'],
+              'PSATitle' => $row['PSATitle'],
+              'PSACode' => $row['PSACode'],
+              'PSACentralCity' => $row['PSACentralCity'],
+              'cities' => explode(", ", $row['allCities']));
+        if($field) {
+          if($field == 'cities') {
+            // Transform the allCities field in a json object
+            $cities = explode(", ", $row['allCities']);
+            echo json_encode($cities);
+          } else {
+            echo $r[$field];
+          }
+        } else {
+          echo json_encode($r);
+        }
+        
+    }
 
-	  mysqli_close($con);
+    mysqli_close($con);
   } else {
-  	echo 'Invalid API key.';
+    echo 'Invalid API key.';
   }
 ?>
